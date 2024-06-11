c388712f3a8483799d928d45c8783680298962fd
Alan Kasis <alankasis@gmail.com>
2024-06-11 19:55:47 -0300

    feat: shuffling and winning features

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const N = 4;
-const pieces = document.querySelectorAll(".puzzle__piece");
+const PUZZLE_SIZE = 500;
+const shuffleButton = document.getElementById("shuffle-button");
+const puzzle = document.getElementById("puzzle");
+const moveSound = new Audio("https://images.chesscomfiles.com/chess-themes/sounds/_MP3_/default/move-self.mp3");
+
+const arraysEqual = (a, b) => {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+  for (let i = 0; i < a.length; ++i) {
+    for (let j = 0; j < a[i].length; j++) {
+      if (a[i][j] !== b[i][j]) return false;
+    }
+  }
+  return true;
+};
+
+const playMoveSound = () => {
+  moveSound.play();
+};
 
 const createLayout = (n) => {
   let layout = [];
@@ -17,45 +36,40 @@ const createLayout = (n) => {
   return layout;
 };
 
+const setPieceStyle = (piece, pieceIndex) => {
+  if (pieceIndex == "empty") {
+    piece.style.background = "transparent";
+  } else {
+    piece.style.backgroundImage = "url(/public/puzzle-image.jpg)";
+    piece.style.backgroundPosition = `-${
+      (pieceIndex % N) * (PUZZLE_SIZE / N)
+    }px -${Math.floor(pieceIndex / N) * (PUZZLE_SIZE / N)}px`;
+  }
+};
+
 const generatePuzzle = () => {
   for (let i = 0; i < layout.length; i++) {
     for (let j = 0; j < layout[i].length; j++) {
       let piece = document.getElementById(`piece-${i}-${j}`);
       let pieceIndex = layout[i][j];
-      if (pieceIndex == "empty") {
-        piece.style.background = "#555555";
-      } else {
-        piece.style.backgroundImage = "url(/public/puzzle-image.jpg)";
-        piece.style.backgroundPosition = `-${(pieceIndex % N) * 125}px -${
-          Math.floor(pieceIndex / N) * 125
-        }px`;
-      }
+      setPieceStyle(piece, pieceIndex);
     }
   }
 };
 
-const layout = createLayout(N);
-generatePuzzle();
+// Animation end event listener to wait for animation end to regenerate puzzle
+const handleAnimationEnd = (piece) => {
+  piece.removeEventListener("animationend", handleAnimationEnd);
+  piece.classList.remove("slide-right", "slide-left", "slide-up", "slide-down");
+  generatePuzzle();
+};
 
-window.addEventListener("click", (e) => {
-  let piece = e.target;
+// Moves piece is movement is possible
+const movePiece = (piece) => {
   let row = Number(piece.id.split("-")[1]);
   let col = Number(piece.id.split("-")[2]);
-  if (layout[row][col] === "empty") {
-    return;
-  }
 
-  // Transition end event listener to wait for animation end to regenerate puzzle
-  const handleAnimationEnd = (event) => {
-    piece.removeEventListener("animationend", handleAnimationEnd);
-    piece.classList.remove(
-      "slide-right",
-      "slide-left",
-      "slide-up",
-      "slide-down"
-    );
-    generatePuzzle();
-  };
+  if (layout[row][col] === "empty") return;
 
   // RIGHT direction movement logic
   if (col < N - 1 && layout[row][col + 1] == "empty") {
@@ -80,6 +94,87 @@ window.addEventListener("click", (e) => {
     return;
   }
   layout[row][col] = "empty";
-  piece.addEventListener("animationend", handleAnimationEnd);
+  piece.addEventListener("animationend", () => handleAnimationEnd(piece));
+  playMoveSound();
+  checkWinner();
   return;
+};
+
+// Function to shuffle the puzzle
+// Should be able to provide a solvable puzzle, so it's based on making n random legal moves
+const shufflePuzzle = (n) => {
+  for (let k = 0; k < n; k++) {
+    // Find the empty slot
+    const emptySlot = findEmptySlot();
+
+    const moveDirection = ["right", "left", "up", "down"];
+    const selectedMove = moveDirection[Math.floor(Math.random() * 4)];
+
+    switch (selectedMove) {
+      case "right":
+        if (emptySlot.col < N - 1)
+          movePieceByCoordinates(emptySlot.row, emptySlot.col + 1);
+        break;
+      case "left":
+        if (emptySlot.col > 0)
+          movePieceByCoordinates(emptySlot.row, emptySlot.col - 1);
+        break;
+      case "up":
+        if (emptySlot.row > 0)
+          movePieceByCoordinates(emptySlot.row - 1, emptySlot.col);
+        break;
+      case "down":
+        if (emptySlot.row < N - 1)
+          movePieceByCoordinates(emptySlot.row + 1, emptySlot.col);
+        break;
+    }
+  }
+};
+
+// Finds the coordinates of the empty slot and stores them in an object
+const findEmptySlot = () => {
+  for (let i = 0; i < layout.length; i++) {
+    for (let j = 0; j < layout[i].length; j++) {
+      if (layout[i][j] === "empty") {
+        return { row: i, col: j };
+      }
+    }
+  }
+  return null;
+};
+
+// Moves selected piece
+const movePieceByCoordinates = (row, col) => {
+  const piece = document.getElementById(`piece-${row}-${col}`);
+  if (piece) movePiece(piece);
+};
+
+// Checks if the current puzzle disposition corresponds to the original image
+const checkWinner = () => {
+  console.log(arraysEqual(layout, solution));
+  if (arraysEqual(layout, solution)) {
+    console.log("Winner!");
+  }
+};
+
+const layout = createLayout(N);
+const solution = createLayout(N);
+
+const startGame = () => {
+  generatePuzzle();
+  shufflePuzzle(5);
+};
+
+export { startGame, movePiece, shufflePuzzle, generatePuzzle, checkWinner };
+
+
+puzzle.addEventListener("click", (e) => {
+  console.log(e.target);
+  movePiece(e.target);
 });
+
+shuffleButton.addEventListener("click", (e) => {
+  shufflePuzzle(100);
+});
+
+startGame();
